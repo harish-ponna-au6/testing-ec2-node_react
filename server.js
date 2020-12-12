e47eecd74b5a7581f5987421e985d57772056a9e
harish-ponna-au6 <harishponna@codalyze.com>
2020-12-12 20:31:47 +0530

    d

--- server.js
+++ server.js
@@ -1,13 +1,11 @@
 const { connect } = require("mongoose");
 const Message = require("./Message");
 const express = require("express");
-const path = require('path')
+const path = require("path");
 
-const app = express()
+const app = express();
 
-const uri = `mongodb+srv://chinnu:aDjOa927F8zGtI9r@cluster0.kuahz.mongodb.net/testing-ec-2?retryWrites=true&w=majority`;
-
-connect(uri, { useNewUrlParser: true, useCreateIndex: true })
+connect("mongodb://localhost:27017/testing-ec-2", { useNewUrlParser: true })
   .then(() => console.log("\x1b[31m%s\x1b[0m", "mongoDB connected"))
   .catch((e) => console.log(e));
 
@@ -24,7 +22,9 @@ app.get("/api", async (_, res) => {
   }
 });
 
-app.use(express.static(path.resolve(__dirname,"client","build")))
-app.get("*",(_,res)=>res.sendFile(path.resolve(__dirname,"client","build","index.html")))
+app.use(express.static(path.resolve(__dirname, "client", "build")));
+app.get("*", (_, res) =>
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+);
 
 app.listen(5000, () => console.log("server started at 8080"));
